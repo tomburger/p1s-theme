881770915d58ebd74a7eed3b80f595924b67fdb4
Tom Burger <tomas@burger.cz>
2020-04-20 22:05:06 +0200

    first edit - remove theme name

--- footer.php
+++ footer.php
@@ -19,11 +19,6 @@
 				printf( esc_html__( 'Proudly powered by %s', 'p1s' ), 'WordPress' );
 				?>
 			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'p1s' ), 'p1s', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
